72a97bf4c9bc80aa62c58511d782558e8b7972d7
Sneha Kshirsagar <94184440+SnehaKsh@users.noreply.github.com>
2022-01-02 15:10:32 +0530

    Add files via upload

---
+++ Problem8.py
@@ -0,0 +1,6 @@
+# Python Program to find area of a circle
+
+radius=int(input("Enter the radius of circle : "))
+pie= 3.1416
+area_of_circle= pie*(radius**2)
+print("The area of given circle is : ", area_of_circle)
