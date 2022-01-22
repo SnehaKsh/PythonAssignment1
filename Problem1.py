f392383f34b19a6bd4febdbd15701215d03c27f5
Sneha Kshirsagar <94184440+SnehaKsh@users.noreply.github.com>
2022-01-22 18:27:58 +0530

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -1,7 +1,7 @@
 #code to find out numbers which are divisible by 7 and multiple of 5, between 1500 and 2700 (both included).
 
 final=[]
-for x in range(1500, 2701:
+for x in range(1500, 2701):
     if (x%7 == 0) and (x%5 ==0):
         final.append(x)
 print(final)
